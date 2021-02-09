498cf4a65aa187b20179cd39f57a77d5be4c8161
Damien Triegaardt <dtriegaardt@outlook.com>
2021-02-09 10:50:41 +0000

    Added react routing to HomePage.js

--- blast/frontend/src/components/HomePage.js
+++ blast/frontend/src/components/HomePage.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router, 
+    Switch, 
+    Route, 
+    Link, 
+    Redirect, 
+} from 'react-router-dom';
+import JoinRoomPage from "./JoinRoomPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -6,6 +15,14 @@ export default class HomePage extends Component {
     }
 
     render() {
-        return <h1>This is the Home Page</h1>;
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/'><h1>This is the Home Page</h1></Route>
+                    <Route exact path='/join' component={JoinRoomPage}></Route>
+                    <Route exact path='/create' component={CreateRoomPage}></Route>
+                </Switch>
+            </Router>
+        );
     }
 }
