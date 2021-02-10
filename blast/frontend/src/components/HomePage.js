094da6e4e4f3d691137e2ec80389d819c8631066
Damien Triegaardt <dtriegaardt@outlook.com>
2021-02-10 18:08:41 +0000

    Added frontend components and styling to home page

--- blast/frontend/src/components/HomePage.js
+++ blast/frontend/src/components/HomePage.js
@@ -9,10 +9,51 @@ import {
 import JoinRoomPage from "./JoinRoomPage";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomPage from "./RoomPage";
+import { Button, Grid, Typography, ButtonGroup } from "@material-ui/core";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        };
+    }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code
+            });
+        });
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3} align="center">
+
+                <Grid item xs={12}>
+                    <Typography variant="h1" component="h1">
+                        blast
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+
+                </Grid>
+
+            </Grid>
+        );
     }
 
     render() {
@@ -22,7 +63,11 @@ export default class HomePage extends Component {
                     <Route path='/join' component={JoinRoomPage} />
                     <Route path='/create' component={CreateRoomPage} />
                     <Route path='/room/:roomCode' component={RoomPage} />
-                    <Route path='/'><h1>This is the Home Page</h1></Route>
+
+                    // If user has a room code stored in state, redirect to room, else go to home page
+                    <Route path='/' render={() => {
+                        return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} />) : this.renderHomePage();
+                    }} />
                 </Switch>
             </Router>
         );
