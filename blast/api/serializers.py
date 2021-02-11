bd1c10fcce7ca1f32e157d65ae419ef1c08bd740
Damien Triegaardt <dtriegaardt@outlook.com>
2021-02-11 12:41:20 +0000

    Added UpdateRoomSerializer to serializers.py

--- blast/api/serializers.py
+++ blast/api/serializers.py
@@ -6,7 +6,17 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('__all__')
 
+
 class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause', 'votes_to_skip')
+
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+
+    code = serializers.CharField(validators=[])
+
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
