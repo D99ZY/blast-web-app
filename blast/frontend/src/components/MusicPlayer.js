db1fc76457e2e09e743842d4f76848382eaed527
Damien Triegaardt <dtriegaardt@outlook.com>
2021-02-16 17:25:32 +0000

    Fixed card sizing issue with renderDefault function in MusicPlayer.js

--- blast/frontend/src/components/MusicPlayer.js
+++ blast/frontend/src/components/MusicPlayer.js
@@ -58,7 +58,7 @@ export default class MusicPlayer extends Component {
                         </Typography>
 
                         <Typography color="textSecondary" varient="subtitle1">
-                            {"Select Song"}
+                            {"Start playing a song on Spotify"}
                         </Typography>
 
                         <div>
