e4d7f934cdc8ffec4feebc069cdec84b23d1a2be
Damien Triegaardt <dtriegaardt@outlook.com>
2021-02-19 13:21:35 +0000

    Updated settings.py

--- blast/urls.py
+++ blast/urls.py
@@ -16,16 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.static import serve
-from django.conf.urls import url
-from django.conf import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('spotify/', include('spotify.urls')),
     path('', include('frontend.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
 ]
